f86db23ec5bd837b4c3c0e55cf47d1941fc9f598
Stas <banfan24@gmail.com>
2021-10-11 17:46:34 +0300

    introduce context

--- helpers/api.ts
+++
@@ -1,5 +0,0 @@
-export const API = {
-    topPage: {
-        find: process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find'
-    }
-}
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import {withLayout} from "../layout/Layout";
 import {GetStaticProps} from "next";
 import axios from "axios";
 import {MenuItem} from "../interfaces/menu.interface";
-import {API} from "../helpers/api";
 
 function Home({ menu }: HomeProps): JSX.Element {
 
@@ -14,7 +13,7 @@ function Home({ menu }: HomeProps): JSX.Element {
             <Button appearance='ghost' arrow='right'>Читать отзывы</Button>
             <Button appearance='ghost' arrow='down'>Читать отзывы</Button>
             <Ptag size='s'>Параграф текста</Ptag>
-            <Ptag>Параграф текста</Ptag>
+            <Ptag>Параграф</Ptag>
             <ul>
                 {
                     menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))
@@ -28,7 +27,7 @@ export default withLayout(Home);
 
 export const getStaticProps: GetStaticProps<HomeProps> = async () => {
     const firstCategory = 0;
-    const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, {
+    const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
         firstCategory
     });
     return {
