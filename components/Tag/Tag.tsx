5c78e2bb88f605861a42bf150684bd186ccaf0b9
Stas <banfan24@gmail.com>
2021-10-18 16:50:20 +0300

    product components

--- components/Tag/Tag.tsx
+++ components/Tag/Tag.tsx
@@ -2,9 +2,9 @@ import {TagProps} from "./Tag.props";
 import styles from './Tag.module.css';
 import cn from "classnames";
 
-export const Tag = ({size = 's', children, color = 'ghost', href, ...props}: TagProps): JSX.Element => {
+export const Tag = ({size = 's', children, color = 'ghost', href, className, ...props}: TagProps): JSX.Element => {
     return (
-        <div className={cn(styles.tag, {
+        <div className={cn(styles.tag, className, {
             [styles.s]: size == 's',
             [styles.m]: size == 'm',
             [styles.ghost]: color == 'ghost',
